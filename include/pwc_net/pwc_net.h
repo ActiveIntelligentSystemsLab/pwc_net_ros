deb3de51ffd0ffd1db82597d10ad32b7773991f0
Hironori Fujimoto <fujimoto@aisl.cs.tut.ac.jp>
2020-03-16 08:25:16 +0000

    Add libsgm_gpu

---
+++ include/pwc_net/pwc_net.h
@@ -0,0 +1,106 @@
+#ifndef PWC_NET__PWC_NET_H_
+#define PWC_NET__PWC_NET_H_
+
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <nodelet/nodelet.h>
+#include <sensor_msgs/Image.h>
+#include <std_msgs/Header.h>
+
+#include <caffe/caffe.hpp>
+#include <caffe/layers/input_layer.hpp>
+
+#include <opencv2/opencv.hpp>
+
+#include <memory>
+#include <string>
+
+namespace pwc_net {
+
+class PwcNet {
+private:
+  const std::string PACKAGE_NAME_ = "pwc_net";
+
+  const std::string SOURCE_IMAGE_BLOB_ = "img0";
+  const std::string DIST_IMAGE_BLOB_ = "img1";
+  const std::string OUTPUT_BLOB_ = "predict_flow_final";
+
+  /**
+   * @brief Used to resize input images. 
+   *
+   * Network require that input image width and height are multiples of this number.
+   */
+  const double RESOLUTION_DIVISOR_ = 64.0;
+
+  /**
+   * @brief Size of resized image for network input layer
+   * 
+   * Network require that input image width and height are multiples of PWCNetNodelet::RESOLUTION_DIVISOR_
+   */
+  int adapted_width_;
+  /**
+   * @brief Size of resized image for network input layer
+   * 
+   * Network require that input image width and height are multiples of PWCNetNodelet::RESOLUTION_DIVISOR_
+   */
+  int adapted_height_;
+
+  /**
+   * @brief Original size of input image
+   */
+  int target_width_;
+  /**
+   * @brief Original size of input image
+   */
+  int target_height_;
+
+  using d_type_ = float;
+  std::shared_ptr<caffe::Net<d_type_>> net_;
+
+  /**
+   * @brief Generate temporary model file from template
+   * Template model file is contain some variable so replace them to real number
+   * 
+   * @param package_path 
+   */
+  std::string generateTemporaryModelFile(const std::string &package_path);
+
+  /**
+   * @brief Initialize network model
+   * 
+   * @param image_width Width of input images
+   * @param image_height Height of input images
+   */
+  void initializeNetwork(int image_width, int image_height);
+
+  /**
+   * @brief Convert output layer blob to cv::Mat
+   */
+  void outputLayerToCvMat(cv::Mat& optical_flow);
+
+  /**
+   * @brief Set input images To input Layer of network
+   * 
+   * @param source_image 
+   * @param dist_image 
+   */
+  void setImagesToInputLayer(const cv::Mat& source_image, const cv::Mat& dist_image);
+public:
+  /**
+   * @brief Estimate optical flow from source_image to dist_image by PWC-Net
+   *
+   * @param optical_flow CV_32FC2. first channel is optical flow's x-axis component, second is y-axis.
+   * @return false if input images are invalid, such as not same size
+   */
+  bool estimateOpticalFlow
+  (
+    const sensor_msgs::Image& source_image_msg, 
+    const sensor_msgs::Image& dist_image_msg,
+    cv::Mat& optical_flow
+  ); 
+  
+};
+
+} // end of pwc_net namespace
+
+#endif
---
+++ src/pwc_net.cu
@@ -0,0 +1,202 @@
+#include "pwc_net/pwc_net.h"
+
+#include <cv_bridge/cv_bridge.h>
+#include <ros/console.h>
+#include <ros/package.h>
+
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <map>
+#include <vector>
+
+namespace pwc_net{
+
+std::string PwcNet::generateTemporaryModelFile(const std::string &package_path) {
+  std::string model_file_template = package_path + "/model/pwc_net_test.prototxt";
+  ROS_INFO_STREAM_NAMED("libpwc_net", "Loading template of model file: " << model_file_template);
+
+  std::ifstream template_ifstream(model_file_template);
+  if (!template_ifstream.is_open()) {
+    ROS_FATAL_STREAM_NAMED("libpwc_net", "Cannot open template file of model: " << model_file_template);
+    ros::shutdown();
+    exit(EXIT_FAILURE);
+  }
+
+  std::string temporary_model_file = package_path + "/model/tmp_model_file.prototxt";
+  ROS_INFO_STREAM_NAMED("libpwc_net", "Generate temporary model file: " << temporary_model_file);
+
+  std::ofstream temporary_ofstream(temporary_model_file);
+  if (!temporary_ofstream.is_open()) {
+    ROS_FATAL_STREAM_NAMED("libpwc_net", "Cannot open temporary model file: " << temporary_model_file);
+    ros::shutdown();
+    exit(EXIT_FAILURE);
+  }
+
+  std::map<std::string, std::string> replacement_map;
+  replacement_map.emplace("$ADAPTED_WIDTH", std::to_string(adapted_width_));
+  replacement_map.emplace("$ADAPTED_HEIGHT", std::to_string(adapted_height_));
+  replacement_map.emplace("$TARGET_WIDTH", std::to_string(target_width_));
+  replacement_map.emplace("$TARGET_HEIGHT", std::to_string(target_height_));
+  replacement_map.emplace("$SCALE_WIDTH", std::to_string(1.0 * target_width_ / adapted_width_));
+  replacement_map.emplace("$SCALE_HEIGHT", std::to_string(1.0 * target_height_ / adapted_height_));
+
+  for (std::string line; std::getline(template_ifstream, line);) {
+    for (auto iterator = replacement_map.begin(); iterator != replacement_map.end(); iterator++) {
+      size_t replace_start;
+      std::string replaced_text = iterator->first;
+      std::string new_text = iterator->second;
+
+      replace_start = line.find(replaced_text);
+      if (replace_start != std::string::npos)
+        line.replace(replace_start, replaced_text.length(), new_text);
+    }
+
+    line += "\n";
+    temporary_ofstream.write(line.c_str(), static_cast<long>(line.length()));
+  }
+  template_ifstream.close();
+  temporary_ofstream.close();
+
+  return temporary_model_file;
+}
+
+bool PwcNet::estimateOpticalFlow
+(
+  const sensor_msgs::Image& source_image_msg, 
+  const sensor_msgs::Image& dist_image_msg,
+  cv::Mat& optical_flow
+) 
+{
+  // Are input images same size?
+  if (source_image_msg.width != dist_image_msg.width
+    || source_image_msg.height != dist_image_msg.height)
+  {
+    ROS_ERROR_STREAM_NAMED("libpwc_net",
+      "Input images aren't same size!\n" <<
+      "source: " << source_image_msg.width << "x" << source_image_msg.height << "\n" <<
+      "dist: " << dist_image_msg.width << "x" << dist_image_msg.height
+    );
+
+    return false;
+  }
+
+  // Initialize network if not
+  if (!net_)
+    initializeNetwork(dist_image_msg.width, dist_image_msg.height);
+  else if (dist_image_msg.width != target_width_ || dist_image_msg.height != target_height_)
+  {
+    ROS_INFO_STREAM_NAMED("libpwc_net", 
+      "Size of input image is not same to first input image which is used to initialize network.\n" << 
+      "Reinitialize network for new size\n" << 
+      "old: " << target_width_ << "x" << target_height_ << "\n" <<
+      "new: " << dist_image_msg.width << "x" << dist_image_msg.height
+    );
+    initializeNetwork(dist_image_msg.width, dist_image_msg.height);
+  }
+
+  // Convert msg to cv::Mat
+  cv::Mat dist_image;
+  cv::Mat source_image;
+  try 
+  {
+    dist_image = cv_bridge::toCvCopy(dist_image_msg, "bgr8")->image;
+    source_image = cv_bridge::toCvCopy(source_image_msg, "bgr8")->image;
+  }
+  catch(const cv_bridge::Exception& exception) 
+  {
+    ROS_ERROR_STREAM_NAMED("libpwc_net", exception.what());
+    return false;
+  }
+
+  // Convert cv::Mat to float and set to input layer
+  source_image.convertTo(dist_image, CV_32FC3);
+  dist_image.convertTo(dist_image, CV_32FC3);
+  setImagesToInputLayer(source_image, dist_image);
+
+  net_->Forward();
+
+  outputLayerToCvMat(optical_flow);
+
+  return true;
+}
+
+void PwcNet::initializeNetwork(int image_width, int image_height) {
+  ROS_INFO_STREAM_NAMED("libpwc_net", "Start network initialization\n"
+    << "input image size: " << image_width << "x" << image_height);
+
+  if (image_width <= 0 || image_height <= 0)
+  {
+    ROS_FATAL_STREAM_NAMED("libpwc_net", "Invalid size is specified to network initialization!\n" 
+      << "Specified value: " << image_width << "x" << image_height);
+      ros::shutdown();
+      std::exit(EXIT_FAILURE);
+  }
+
+  target_width_ = image_width;
+  target_height_ = image_height;
+  adapted_width_ = static_cast<int>(std::ceil(target_width_ / RESOLUTION_DIVISOR_) * RESOLUTION_DIVISOR_);
+  adapted_height_ = static_cast<int>(std::ceil(target_height_ / RESOLUTION_DIVISOR_) * RESOLUTION_DIVISOR_);
+
+  std::string package_path = ros::package::getPath(PACKAGE_NAME_);
+  if (package_path.empty()) {
+    ROS_FATAL_STREAM_NAMED("libpwc_net", "Package not found: " << PACKAGE_NAME_);
+    ros::shutdown();
+    std::exit(EXIT_FAILURE);
+  }
+
+  std::string temporary_model_file = generateTemporaryModelFile(package_path);
+
+  ROS_INFO_NAMED("libpwc_net", "Loading temporary model file");
+  net_.reset(new caffe::Net<d_type_>(temporary_model_file, caffe::TEST));
+
+  std::string trained_file = package_path + "/model/pwc_net.caffemodel";
+  ROS_INFO_STREAM_NAMED("libpwc_net", "Loading trained file: " << trained_file);
+  net_->CopyTrainedLayersFrom(trained_file);
+
+  ROS_INFO_STREAM_NAMED("libpwc_net", "Network initialization is finished");
+}
+
+void PwcNet::outputLayerToCvMat(cv::Mat& optical_flow)
+{
+  const boost::shared_ptr<caffe::Blob<d_type_>> output_blob = net_->blob_by_name(OUTPUT_BLOB_);
+
+  cv::Mat channels[2];
+
+  int height = output_blob->shape(2);
+  int width = output_blob->shape(3);
+
+  channels[0].create(cv::Size(width, height), CV_32FC1);
+  channels[1].create(cv::Size(width, height), CV_32FC1);
+
+  int total_pixel = height * width;
+  const float* x_channel = output_blob->cpu_data();
+  const float* y_channel = x_channel + total_pixel;
+  size_t channel_size = sizeof(float) * total_pixel;
+  std::memcpy(channels[0].data, x_channel, channel_size);
+  std::memcpy(channels[1].data, y_channel, channel_size);
+
+  cv::merge(channels, 2, optical_flow);
+}
+
+void PwcNet::setImagesToInputLayer(const cv::Mat& source_image, const cv::Mat& dist_image) {
+  std::vector<cv::Mat> channels;
+  size_t channel_size = source_image.cols * source_image.rows;
+
+  // Set source image
+  cv::split(source_image, channels); // Split to BGR channels
+  d_type_ *input_layer_blob = net_->blob_by_name(SOURCE_IMAGE_BLOB_)->mutable_cpu_data();
+  // Store each channels to blob
+  for (int i = 0; i < 3; i++)
+    memcpy(input_layer_blob + (channel_size * i), channels[i].ptr<d_type_>(), channel_size * sizeof(float));
+
+  // Set dist image
+  cv::split(dist_image, channels);
+  input_layer_blob = net_->blob_by_name(DIST_IMAGE_BLOB_)->mutable_cpu_data();
+  for (int i = 0; i < 3; i++)
+    memcpy(input_layer_blob + (channel_size * i), channels[i].ptr<d_type_>(), channel_size * sizeof(float));
+
+  caffe::Caffe::set_mode(caffe::Caffe::GPU);
+}
+
+}
